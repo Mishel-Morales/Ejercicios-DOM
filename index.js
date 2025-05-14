ff803ddaeb0c43cca7a4fee0d40f6e0353e99960
Mishel Morales <mmejiam1825@gmail.com>
2025-05-14 00:47:38 -0600

    Se completo ejercicio 2 colocar en rojo el numero y se agregaron los botones en index.html y se agregaron cambios de los botones en index.js

--- index.js
+++ index.js
@@ -4,22 +4,34 @@ let resetear = document.querySelector('.cero')
 let numero  = document.querySelector('h1')
 let body = document.querySelector('body')
 let caja = document.querySelector('.container')
+let botones = document.querySelectorAll('.btn')
+console.log(botones)
+// let btnBlue = document.querySelector('#blue')
+// let btnGreen = document.querySelector('#green')
+// let btnRed = document.querySelector('#red')
+// let btnYellow = document.querySelector('#yellow')
 
 let total = ''
 let tecla = ''
 let digito = ''
-
 let space = '  '
 let igual = '='
 
 const codigo = (n) =>{
+    let cambio = document.createElement('span')
+    cambio.classList.add('text-danger')
     digito = n
     if(digito.charCodeAt(0) >=48 && digito.charCodeAt(0) <= 57){
-        caja.append(digito)
+        cambio.append(digito)
+        caja.append(cambio)
         caja.append(igual)
         caja.append(digito.charCodeAt(0))
         caja.append(space)
     }
+}
+
+const color = (value) =>{
+
 }
 
 incrementar.addEventListener('click', (event) => {
@@ -58,4 +70,8 @@ body.addEventListener('keyup', (event) => {
     } else{
         codigo(event.key)
     }
+})
+
+botones.addEventListener('click', (event) => {
+    color(event.value)
 })
